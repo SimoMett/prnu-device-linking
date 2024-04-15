2f89840ba5d7f16bcc98dcf0803291855048307e
matteo <matt.simonetti@hotmail.it>
2024-04-15 19:02:18 +0200

    Trying to get correct path for video files

--- generate-video-sequences.py
+++ generate-video-sequences.py
@@ -1,5 +1,9 @@
+import ffmpeg
+import os
+import glob
+
 devices_list = [
-    ("D22_Apple_iPadAir", "D24_Apple"),
+    ("D02_Apple_iPhoneX", "D24_Apple_iPadAir"),
     ("D19_Google_Pixel3a", "D34_Google_Pixel5"),
     ("D05_Huawei_P9Lite", "D33_Huawei_Mate10Pro"),
     ("D40_Motorola_MotoG9Plus", "D39_Motorola_MotoG5"),
@@ -7,7 +11,7 @@ devices_list = [
     ("D38_Xiaomi_Redmi5Plus", "D04_Xiaomi_RedmiNote8T")
 ]
 devices_list_plain = [
-    "D22_Apple_iPadAir", "D24_Apple",
+    "D02_Apple_iPhoneX", "D24_Apple_iPadAir",
     "D19_Google_Pixel3a", "D34_Google_Pixel5",
     "D05_Huawei_P9Lite", "D33_Huawei_Mate10Pro",
     "D40_Motorola_MotoG9Plus", "D39_Motorola_MotoG5",
@@ -23,21 +27,29 @@ devices_list2 = [
     (38, 4)
 ]
 locations_cnt = 7
-scenes_cnt = 6
+scenes_cnt = 5  # used to be 6, but dataset is lacking
+
+#              A   G   A  H   A   M   A   S   A   X
+sequence_a = (2, 19, 2, 5, 2, 40, 2, 32, 2, 38)
 
 
-def generate_videos(device, i, j):
-    #for dev in devices_list_plain:
-        #print(dev + "/L" + str(i + 1) + "/S" + str(j + 1) + "/")
-    print(device + "/L" + str(i + 1) + "/S" + str(j + 1) + "/")
+def generate_videos(sequence, i, j):
+    assert i != 0 and j != 0
+
+    print("Concatenating the following videos:")
+    [print(get_video_path(e, i, j)) for e in sequence]
     return
 
 
+def get_video_path(device, l, s):
+    result = glob.glob("Dataset/D" + "{:02d}".format(device) + "_*/Nat/jpeg-h264/L" + str(l) + "/S" + str(s) + "/*")
+    # assert result != []
+    return result
+
+
 def main():
-    for i in range(locations_cnt):
-        for j in range(scenes_cnt):
-            for dev in devices_list_plain:
-                generate_videos(dev, i, j)
+    generate_videos(sequence_a, 1, 2)
+
     return
 
 

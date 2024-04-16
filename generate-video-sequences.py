5a94e690d307810a5c5b702c1fe9572deadafa1c
matteo <matt.simonetti@hotmail.it>
2024-04-16 13:56:10 +0200

    Refactoring and debug stuff

--- generate-video-sequences.py
+++ generate-video-sequences.py
@@ -18,7 +18,7 @@ def extract_clip(input_file: str, output: str, start=0, duration=10):
 # I'll try to doc it
 def concatenate_videos(videos: list, output: str):
     default_start = 0
-    default_duration = 3
+    default_duration = 1
 
     # For each video sources I have to trim the video and create a split filter using 'filter_multi_output'
     # and then count every stream occurrence used during encoding process to avoid using the same stream multiple times
@@ -27,6 +27,7 @@ def concatenate_videos(videos: list, output: str):
     splits_map = dict.fromkeys(videos_set)
     for v in splits_map.keys():
         if v is not None:
+            assert os.path.isfile(v)
             splits_map[v] = (
                 [ffmpeg.input(v).trim(start=default_start, end=default_duration).filter_multi_output("split"), 0])
         else:
@@ -48,10 +49,10 @@ def concatenate_videos(videos: list, output: str):
     ffmpeg.concat(*clips).output(output).overwrite_output().run()
 
 
-def generate_video_sequence(sequence, l, s, output):
+def generate_video_sequence(sequence, l, s, output: str):
+    print("Generating", output+":")
     assert l != 0 and s != 0
 
-    print("Generating sequence:")
     file_paths = [get_video_path(e, l, s)[0] if get_video_path(e, l, s) != [] else None for e in sequence]
     concatenate_videos(file_paths, output)
     return
@@ -75,22 +76,29 @@ devices_pairs = [
     (38, 4)
 ]
 sequences = [
-    (2,  19,  2,  5,  2, 40,  2, 32,  2, 38),
+    (2, 19, 2, 5, 2, 40, 2, 32, 2, 38),
     (24, 34, 24, 33, 24, 39, 24, 17, 24, 4)
-#     A   G   A   H   A   M  A   S   A   X
+    # A   G   A   H   A   M  A   S   A   X
 ]
 
 
 def main():
     os.makedirs("output", exist_ok=True)
-    for l in range(1, locations_cnt + 1):
-        for s in range(1, scenes_cnt + 1):
-            for d in range(brand_devices_cnt):
-                output = "output/Seq{:d}_Clip_L{:02d}S{:02d}.mp4".format(d+1, l, s)
-                print("Generating", output)
+    for d in range(brand_devices_cnt):
+        for l in range(1, locations_cnt + 1):
+            for s in range(1, scenes_cnt + 1):
+                output = "output/Seq{:d}_Clip_L{:02d}S{:02d}.mp4".format(d + 1, l, s)
                 generate_video_sequence(sequences[d], l, s, output)
     return
 
 
+def debug_main():
+    l = 1
+    s = 1
+    d = 1
+    output = "output/Seq{:d}_Clip_L{:02d}S{:02d}.mp4".format(d + 1, l, s)
+    generate_video_sequence(sequences[d], l, s, output)
+
+
 if __name__ == "__main__":
-    main()
+    debug_main()

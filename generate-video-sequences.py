62e1a71dbcd4b17a7d7ea0350811aae8c43e8f47
matteo <matt.simonetti@hotmail.it>
2024-04-16 10:05:15 +0200

    Implemented extract_clip and concatenate_videos functions

--- generate-video-sequences.py
+++ generate-video-sequences.py
@@ -1,22 +1,43 @@
 import ffmpeg
-import os
 import glob
 
 locations_cnt = 7
 scenes_cnt = 5  # used to be 6, but dataset is lacking
 
-#              A   G   A  H   A   M   A   S   A   X
+#             A   G  A  H  A   M  A   S  A   X
 sequence_a = (2, 19, 2, 5, 2, 40, 2, 32, 2, 38)
 
-def concatenate_videos(videos: list):
-    print(videos)
+
+# extract clip WITHOUT audio, but for the purpose of this project it's fine
+def extract_clip(input_file: str, output: str, start=0, duration=10):
+    (
+        ffmpeg
+        .input(input_file)
+        .trim(start=start, duration=duration)
+        .output(output)
+        .run()
+    )
+
+
+def concatenate_videos(videos: list, output: str):
+    clips = []
+    for v in videos:
+        if v is not None:
+            clips.append(ffmpeg.input(v).trim(start=0, end=4))
+    (
+        ffmpeg
+        .concat(*clips)
+        .output(output)
+        .run()
+    )
+
 
 def generate_videos(sequence, i, j):
     assert i != 0 and j != 0
 
     print("Generating sequence:")
     file_paths = [get_video_path(e, i, j)[0] if get_video_path(e, i, j) != [] else None for e in sequence]
-    concatenate_videos(file_paths)
+    concatenate_videos(file_paths, "test.mp4")
     return
 
 

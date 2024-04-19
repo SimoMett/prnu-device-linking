6757a6e6208bb8c733e63c39c28e6b4d3d26df44
matteo <matt.simonetti@hotmail.it>
2024-04-19 17:27:56 +0200

    Trying to find peaks

--- scene-matching.py
+++ scene-matching.py
@@ -25,7 +25,7 @@ def extract_frame(video_file, frame_number):
 
 def pick_frames(video: cv2.VideoCapture, interval):
     frames_count = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
-    print(frames_count, "at", int(video.get(cv2.CAP_PROP_FPS)), "fps")
+    # print(frames_count, "at", int(video.get(cv2.CAP_PROP_FPS)), "fps")
 
     picked_frames = []
     for i in range(0, frames_count, interval):
@@ -36,32 +36,36 @@ def pick_frames(video: cv2.VideoCapture, interval):
 
 
 def stability_measure(frame1, frame2, lpf_cutoff=16):
-    filtered_frame1 = low_pass_filter.apply_lp_filter_grayscale(frame1, lpf_cutoff)
-    filtered_frame2 = low_pass_filter.apply_lp_filter_grayscale(frame2, lpf_cutoff)
+    if lpf_cutoff == 0:
+        filtered_frame1 = frame1
+        filtered_frame2 = frame2
+    else:
+        filtered_frame1 = low_pass_filter.apply_lp_filter_grayscale(frame1, lpf_cutoff)
+        filtered_frame2 = low_pass_filter.apply_lp_filter_grayscale(frame2, lpf_cutoff)
     return np.linalg.norm((filtered_frame1 - filtered_frame2) ** 2)
 
 
+def find_peaks(values, threshold): # FIXME not always working
+    peaks = []
+    for i in range(len(values)):
+        if values[i] > threshold:
+            peaks.append(i)
+    return peaks
+
+
 def main():
-    lp_cutoff = 16 # using 16 for 1080p frames. This parameter has to be adjusted for other resolutions
-    frames = pick_frames(cv2.VideoCapture("Dataset/D34_Google_Pixel5/Nat/jpeg-h264/L5/S4/D34_L5S4C4.mp4"), 8)
+    lp_cutoff = 0  # using 16 for 1080p frames. This parameter has to be adjusted for other resolutions
+    # using 0 for no LP filter
+
+    frames = pick_frames(cv2.VideoCapture("output/Seq1_Clip_L05S04.mp4"), 3)
     values = []
     for i in range(len(frames) - 1):
         values.append(stability_measure(frames[i], frames[i + 1], lp_cutoff))
     plt.subplot(2, 1, 1)
-    plt.title("D34_L5S4C4.mp4")
-    plt.plot([i for i in range(len(values))], values, color="red")
-    plt.grid()
-
-    frames = pick_frames(cv2.VideoCapture("output/Seq1_Clip_L05S04.mp4"), 2)
-    values = []
-    for i in range(len(frames) - 1):
-        values.append(stability_measure(frames[i], frames[i + 1], lp_cutoff))
-    plt.subplot(2, 1, 2)
     plt.title("Seq1_Clip_L05S04.mp4")
     plt.plot([i for i in range(len(values))], values, color="blue")
+    print(find_peaks(values, np.std(values)*3))
     plt.grid()
-    #values_diffs = [values[i] - values[i + 1] for i in range(len(values) - 1)]
-    # plt.plot([i for i in range(len(values)-1)], values_diffs, color="blue")
 
     plt.show()
     return

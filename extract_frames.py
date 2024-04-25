b463b779797e1700597a7299db23fb0dffc4ffbd
matteo <matt.simonetti@hotmail.it>
2024-04-25 13:18:21 +0200

    Better outputting

--- extract_frames.py
+++ extract_frames.py
@@ -1,10 +1,11 @@
 import sys
+import os
+import glob
 import cv2
 from PIL import Image
 
 
 def extract_frame(video_capture, frame_number):
-    # cv2.VideoCapture(video_file)
     mp4file = video_capture
 
     mp4file.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
@@ -21,6 +22,10 @@ def sequence_from_groundtruth(video_capture):
     tot_frames = int(video_capture.get(cv2.CAP_PROP_FRAME_COUNT))
     print("Frames count", tot_frames)
 
+    # This offset is needed because all the videos have 3009 frames
+    #  which is weird because every video should be 100 seconds long at 30fps, i.e. 3000 frames long.
+    #  Maybe some floating errors?
+    # Correction: there are also videos at 25 fps and 2504 frames long
     offset = 10
     return [i for i in range(offset, tot_frames, fps * seconds_between_clips)]
 
@@ -29,18 +34,24 @@ def extract_frames(video_cap, sequence):
     return [extract_frame(video_cap, f)[1] for f in sequence]
 
 
-if __name__ == "__main__":
-    video_file = sys.argv[1]
+def export_pngs(video_file):
     print(video_file)
     mp4file = cv2.VideoCapture(video_file)
     seq = sequence_from_groundtruth(mp4file)
     print(seq)
     frames = extract_frames(mp4file, seq)
     assert len(frames) == 10
+
+    origin_name = video_file.split('/')[-1].replace(".mp4", "")
+    os.makedirs(origin_name, exist_ok=True)
     for i in range(len(frames)):
-        origin_name = video_file.split('/')[-1].replace(".mp4", "_Frame")
-        filename = origin_name + str(seq[i]) + ".png"
+        filename = origin_name + "/frame" + str(seq[i]) + ".png"
         # print(filename)
         # continue # decomment for dry run
         png_image = Image.fromarray(frames[i])
         png_image.save(filename)
+
+
+if __name__ == "__main__":
+    for mp4 in glob.glob(sys.argv[1]):
+        export_pngs(mp4)

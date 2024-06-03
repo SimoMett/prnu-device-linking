3b095265faf43d885aad4c4ca1514c0732c5f050
SimoMett <matt.simonetti@hotmail.it>
2024-06-03 14:22:07 +0200

    Implemented modified version of extract_multiple_aligned

--- prnu-extract-fingerprints.py
+++ prnu-extract-fingerprints.py
@@ -2,14 +2,16 @@ import pickle
 import re
 import sys
 import os
-from multiprocessing import Pool
-
+from multiprocessing import Pool, cpu_count
 import cv2
 import numpy as np
 import pickle as pk
+from tqdm import tqdm
 import params
 import prnu
 from extract_frames import extract_frames
+from prnu import inten_sat_compact, noise_extract_compact, inten_scale, saturation, rgb2gray, zero_mean_total, \
+    wiener_dft
 from scene_detect import sequence_from_scenedetect
 
 
@@ -52,19 +54,121 @@ def save_results(video_path, aligned_cc, stats_cc, pce_rot, stats_pce):
 def pce_rot_func(_fp_k, _res_w):
     return prnu.pce(prnu.crosscorr_2d(_fp_k, _res_w))['pce']
 
-def prnu_quality_test(frames, processes):
-    block_a = frames[:int(len(frames) / 2)]
-    block_b = frames[int(len(frames) / 2):]
-    print("Computing fingerprints..")
-    fp_a = prnu.extract_multiple_aligned(block_a, processes=processes)
-    fp_b = prnu.extract_multiple_aligned(block_b, processes=processes)
 
-    print("Peak to correlation energy")
-    pce = prnu.pce(prnu.crosscorr_2d(fp_a, fp_b))['pce']
+def extract_and_test_multiple_aligned(imgs: list, levels: int = 4, sigma: float = 5, processes: int = None,
+                                      batch_size=cpu_count(), tqdm_str: str = '') -> np.ndarray:
+    """
+    @author: Luca Bondi (luca.bondi@polimi.it)
+    @author: Paolo Bestagini (paolo.bestagini@polimi.it)
+    @author: Nicolò Bonettini (nicolo.bonettini@polimi.it)
+    Politecnico di Milano 2018
+        Modified by Matteo Simonetti
+
+    Extract PRNU from a list of images. Images are supposed to be the same size and properly oriented
+    :param tqdm_str: tqdm description (see tqdm documentation)
+    :param batch_size: number of parallel processed images
+    :param processes: number of parallel processes
+    :param imgs: list of images of size (H,W,Ch) and type np.uint8
+    :param levels: number of wavelet decomposition levels
+    :param sigma: estimated noise power
+    :return: PRNU
+    """
+    assert (isinstance(imgs[0], np.ndarray))
+    assert (imgs[0].ndim == 3)
+    assert (imgs[0].dtype == np.uint8)
+
+    h, w, ch = imgs[0].shape
+
+    RPsum = np.zeros((h, w, ch), np.float32)
+    NN = np.zeros((h, w, ch), np.float32)
+
+    if processes is None or processes > 1:
+        RPsum_a = np.zeros((h, w, ch), np.float32)
+        NN_a = np.zeros((h, w, ch), np.float32)
+        # First half of imgs
+        with imgs[:int(len(imgs) / 2)] as block_a:
+            args_list = []
+            for im in block_a:
+                args_list += [(im, levels, sigma)]
+            pool = Pool(processes=processes)
+
+            for batch_idx0 in tqdm(np.arange(start=0, step=batch_size, stop=len(block_a)), disable=tqdm_str == '',
+                                   desc=(tqdm_str + ' (1/2)'), dynamic_ncols=True):
+                nni = pool.map(inten_sat_compact, args_list[batch_idx0:batch_idx0 + batch_size])
+                for ni in nni:
+                    NN_a += ni
+                del nni
+
+            for batch_idx0 in tqdm(np.arange(start=0, step=batch_size, stop=len(block_a)), disable=tqdm_str == '',
+                                   desc=(tqdm_str + ' (2/2)'), dynamic_ncols=True):
+                wi_list = pool.map(noise_extract_compact, args_list[batch_idx0:batch_idx0 + batch_size])
+                for wi in wi_list:
+                    RPsum_a += wi
+                del wi_list
+
+        # Second half of imgs
+        RPsum_b = np.zeros((h, w, ch), np.float32)
+        NN_b = np.zeros((h, w, ch), np.float32)
+        with imgs[int(len(imgs) / 2):] as block_b:
+            args_list = []
+            for im in block_b:
+                args_list += [(im, levels, sigma)]
+            pool = Pool(processes=processes)
+
+            for batch_idx0 in tqdm(np.arange(start=0, step=batch_size, stop=len(block_b)), disable=tqdm_str == '',
+                                   desc=(tqdm_str + ' (1/2)'), dynamic_ncols=True):
+                nni = pool.map(inten_sat_compact, args_list[batch_idx0:batch_idx0 + batch_size])
+                for ni in nni:
+                    NN_b += ni
+                del nni
+
+            for batch_idx0 in tqdm(np.arange(start=0, step=batch_size, stop=len(block_b)), disable=tqdm_str == '',
+                                   desc=(tqdm_str + ' (2/2)'), dynamic_ncols=True):
+                wi_list = pool.map(noise_extract_compact, args_list[batch_idx0:batch_idx0 + batch_size])
+                for wi in wi_list:
+                    RPsum_b += wi
+                del wi_list
+        pool.close()
+
+    else:  # Single process
+        # First half
+        block_a = imgs[:int(len(imgs) / 2)]
+        RPsum_a = np.zeros((h, w, ch), np.float32)
+        NN_a = np.zeros((h, w, ch), np.float32)
+        for im in tqdm(block_a, disable=tqdm_str is None, desc="1/2"+tqdm_str, dynamic_ncols=True):
+            RPsum_a += noise_extract_compact((im, levels, sigma))
+            NN_a += (inten_scale(im) * saturation(im)) ** 2
+
+        # Second half
+        block_b = imgs[int(len(imgs) / 2):]
+        RPsum_b = np.zeros((h, w, ch), np.float32)
+        NN_b = np.zeros((h, w, ch), np.float32)
+        for im in tqdm(block_b, disable=tqdm_str is None, desc="2/2"+tqdm_str, dynamic_ncols=True):
+            RPsum_b += noise_extract_compact((im, levels, sigma))
+            NN_b += (inten_scale(im) * saturation(im)) ** 2
+
+    K_a = RPsum_a / (NN_a + 1)
+    K_a = rgb2gray(K_a)
+    K_a = zero_mean_total(K_a)
+    K_a = wiener_dft(K_a, K_a.std(ddof=1)).astype(np.float32)
+    K_b = RPsum_b / (NN_b + 1)
+    K_b = rgb2gray(K_b)
+    K_b = zero_mean_total(K_b)
+    K_b = wiener_dft(K_b, K_b.std(ddof=1)).astype(np.float32)
+    pce = prnu.pce(prnu.crosscorr_2d(K_a, K_b))['pce']
     if pce < 60:
         print("Warning: low PCE found:", pce)
     else:
-        print(pce)
+        print("PCE:", pce)
+
+    RPsum = RPsum_a + RPsum_b
+    NN = NN_a + NN_b
+
+    K = RPsum / (NN + 1)
+    K = rgb2gray(K)
+    K = zero_mean_total(K)
+    K = wiener_dft(K, K.std(ddof=1)).astype(np.float32)
+    return K
 
 
 def procedure(video_path: str):

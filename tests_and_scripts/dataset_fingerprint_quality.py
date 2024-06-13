dd415a4bdf0dc384b23c1afd6a3be6084dd5bab2
SimoMett <matt.simonetti@hotmail.it>
2024-06-13 14:34:12 +0200

    Removed prints

--- tests_and_scripts/dataset_fingerprint_quality.py
+++ tests_and_scripts/dataset_fingerprint_quality.py
@@ -7,8 +7,6 @@ if __name__ == "__main__":
     #s = sys.argv[1]
     #procedure(s)
     for s in glob.glob("Dataset/D*/Nat/jpeg-h264/L5/S2/*.mp4"):
-        print(s)
         procedure(s)
     for s in glob.glob("Dataset/D*/Nat/jpeg-h264/L5/S2/*.MOV"):
-        print(s)
         procedure(s)

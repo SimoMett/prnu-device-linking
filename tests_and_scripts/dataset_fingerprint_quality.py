29b3af7cc5379efdcfcbdfc0e7e538700fd6aa6e
matteo <matt.simonetti@hotmail.it>
2024-06-14 15:55:56 +0200

    Fixed imports

--- tests_and_scripts/dataset_fingerprint_quality.py
+++ tests_and_scripts/dataset_fingerprint_quality.py
@@ -1,11 +1,8 @@
 import glob
-import sys
-from tests_and_scripts.prnu_fingerprint_quality import procedure
+from prnu_fingerprint_quality import procedure
 
 
 if __name__ == "__main__":
-    #s = sys.argv[1]
-    #procedure(s)
     for s in glob.glob("Dataset/D*/Nat/jpeg-h264/L5/S2/*.mp4"):
         procedure(s)
     for s in glob.glob("Dataset/D*/Nat/jpeg-h264/L5/S2/*.MOV"):

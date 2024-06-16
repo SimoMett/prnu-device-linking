be290b9e67dbe497a345d15e3b5598574464e128
matteo <matt.simonetti@hotmail.it>
2024-06-16 17:16:39 +0200

    Saving PCE test results

--- dataset_fingerprint_quality.py
+++ dataset_fingerprint_quality.py
@@ -3,12 +3,13 @@ from prnu_fingerprint_quality import procedure
 
 
 def save_results(file, pce: list):
-    with open("Dataset/dataset_dstab_tests.txt", "w") as f:
-        f.write(",".join([file]+pce))
+    with open("Dataset/dataset_dstab_tests.csv", "a") as f:
+        f.write(",".join([file]+[str(p) for p in pce])+"\n")
 
 
 if __name__ == "__main__":
-    for s in glob.glob("Dataset/D*/Nat/jpeg-h264/L*/S*/*.mp4"):
+    base_str = "Dataset/D*/Nat/jpeg-h264/L5/S2/*"
+    for s in glob.glob(base_str+".mp4"):
         save_results(s, procedure(s))
-    for s in glob.glob("Dataset/D*/Nat/jpeg-h264/L*/S*/*.MOV"):
+    for s in glob.glob(base_str+".MOV"):
         save_results(s, procedure(s))

e6e3b512a49db5abc03a296e8875c9a05add43c7
matteo <matt.simonetti@hotmail.it>
2024-07-17 21:28:40 +0200

    Refactoring and fixed bug

--- compute_roc.py
+++ compute_roc.py
@@ -6,21 +6,20 @@ import prnu_extract_fingerprints
 
 
 def get_roc_stats_by_threshold(ground_truth, pce_rot, threshold):
-    fp = 0
-    tn = 0
-    tp = 0
-    fn = 0
+    fp, tp, fn, tn = 0, 0, 0, 0
     for j in range(pce_rot.shape[0]):
         for i in range(j, pce_rot.shape[1] - 1):
             print(int(pce_rot[i, j]), ground_truth[i, j])
-            if int(pce_rot[i, j]) > threshold and not ground_truth[i, j]:
-                fp += 1
-            elif int(pce_rot[i, j]) < threshold and not ground_truth[i, j]:
-                tn += 1
-            elif int(pce_rot[i, j]) > threshold and ground_truth[i, j]:
-                tp += 1
-            elif int(pce_rot[i, j]) > threshold and not ground_truth[i, j]:
-                fn += 1
+            if pce_rot[i, j] > threshold:
+                if ground_truth[i, j]:
+                    tp += 1
+                else:
+                    fp += 1
+            else:
+                if not ground_truth[i, j]:
+                    tn += 1
+                else:
+                    fn += 1
     return fp, tp, fn, tn
 
 

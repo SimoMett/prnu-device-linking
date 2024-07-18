cd8f872cef37bf50cdcfe976cf2ca61be6d1c970
matteo <matt.simonetti@hotmail.it>
2024-07-18 16:03:23 +0200

    Added plot_roc function

--- roc_stats.py
+++ roc_stats.py
@@ -1,8 +1,20 @@
 import pickle
 import glob
+from matplotlib import pyplot as plt
 
 import prnu
 import prnu_extract_fingerprints
+
+
+def plot_roc(pickle_file):
+    with open(pickle_file, "rb") as file:
+        _, _, pce_rot, stats = pickle.load(file)
+        plt.title('Receiver Operating Characteristic')
+        plt.plot(stats['fpr'], stats['tpr'], 'b', label='AUC = %0.2f' % stats['auc'])
+        plt.legend(loc='lower right')
+        plt.ylabel('True Positive Rate')
+        plt.xlabel('False Positive Rate')
+        plt.show()
 
 
 def get_roc_stats_by_threshold(ground_truth, pce_rot, threshold):
@@ -22,17 +34,18 @@ def get_roc_stats_by_threshold(ground_truth, pce_rot, threshold):
     return fp, tp, fn, tn
 
 
-pickles = glob.glob("Hybrid Dataset/output/Video_Seq*/full_results.pickle")
-for pkl in sorted(pickles):
-    print(pkl)
-    with open(pkl, "rb") as file:
-        _, _, pce_rot, stats = pickle.load(file)
+if __name__ == "__main__":
+    pickles = glob.glob("Hybrid Dataset/output/Video_Seq*/full_results.pickle")
+    for pkl in sorted(pickles):
+        print(pkl)
+        with open(pkl, "rb") as file:
+            _, _, pce_rot, stats = pickle.load(file)
 
-        clips_seq = prnu_extract_fingerprints.devs_sequences[1]
-        ground_truth = prnu.gt(clips_seq, clips_seq)
+            clips_seq = prnu_extract_fingerprints.devs_sequences[1]
+            ground_truth = prnu.gt(clips_seq, clips_seq)
 
-        fp, tp, fn, tn = get_roc_stats_by_threshold(ground_truth, pce_rot, 60)
-        print("FPR:", fp / (fp + tn), "TPR:", tp / (tp + fn))
-        with open(pkl.replace("full_results.pickle", "fpr-tpr-60.csv"), "w") as csv:
-            csv.write("FPR, " + str(fp / (fp + tn))+"\n")
-            csv.write("TPR, " + str(tp / (tp + fn)))
+            fp, tp, fn, tn = get_roc_stats_by_threshold(ground_truth, pce_rot, 60)
+            print("FPR:", fp / (fp + tn), "TPR:", tp / (tp + fn))
+            with open(pkl.replace("full_results.pickle", "fpr-tpr-60.csv"), "w") as csv:
+                csv.write("FPR, " + str(fp / (fp + tn)) + "\n")
+                csv.write("TPR, " + str(tp / (tp + fn)))
